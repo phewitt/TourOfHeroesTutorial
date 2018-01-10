af2b89031210080bef5bdd6488ed2cefdbdae117
Parker Hewitt <Hewittparker@gmail.com>
2018-01-10 13:46:55 -0700

    finished routing and refactor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
+
 @Component({
   selector: "app-heroes",
   templateUrl: "./heroes.component.html",
@@ -8,10 +9,17 @@ import { HEROES } from "../mock-heroes";
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() {}
-  ngOnInit() {}
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
